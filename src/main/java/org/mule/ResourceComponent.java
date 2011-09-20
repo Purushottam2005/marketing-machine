9c8ecabf9af8b53c105d833cf9a523698c5fc096
Dirk Olmes <dirk.olmes@mulesoft.com>
2011-09-20 03:26:51 +0200

    now that the port number is dynamic, fill in the correct port number into the form

--- src/main/java/org/mule/ResourceComponent.java
+++ src/main/java/org/mule/ResourceComponent.java
@@ -10,12 +10,31 @@
 
 package org.mule;
 
+import org.mule.util.IOUtils;
+import org.mule.util.PropertiesUtils;
+
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Properties;
 
 public class ResourceComponent
 {
-    public InputStream getResource(Object ignoredPayload)
+    public String getResource(Object ignoredPayload) throws IOException
     {
-        return getClass().getClassLoader().getResourceAsStream("form.html");
+        String httpPort = httpServerPortFromProperties();
+        String template = loadFormTemplate();
+        return template.replaceAll("@@http.server.port@@", httpPort);
+    }
+
+    private String httpServerPortFromProperties() throws IOException
+    {
+        Properties properties = PropertiesUtils.loadProperties("marketing-machine.properties", getClass());
+        return properties.getProperty("http.server.port");
+    }
+
+    private String loadFormTemplate()
+    {
+        InputStream input = getClass().getClassLoader().getResourceAsStream("form.html");
+        return IOUtils.toString(input);
     }
 }
